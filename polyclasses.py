9ce709ed5c938dd0744d8083fc877b6d7672f6ff
David Wilson <mrdavidjwilson@gmail.com>
2013-11-03 14:26:56 +0000

    added print functino for sparse polynomials

--- polyclasses.py
+++ polyclasses.py
@@ -13,12 +13,11 @@ class DensePoly:
         polystr = ''
         n=len(self.coeffs)
         for i in xrange(1,n):
-            print i
             polystr += str(self.coeffs[n-i])
             polystr += 'x^'+str(n-i) + '+'
         polystr += str(self.coeffs[0])
         return polystr
-
+print("f = DensePoly([3,2,5]):")
 f = DensePoly([3,2,5])
 print(f.degree())
 print(f.printpoly())
@@ -38,5 +37,17 @@ class SparsePoly:
                 deg = self.coeffpairs[i][0]
         return deg
 
+    def printpoly(self):
+        polystr = ''
+        n=len(self.coeffpairs)
+        for i in xrange(n):
+            if self.coeffpairs[i][0]==0:
+                polystr += str(self.coeffpairs[i][1]) + '+'
+            else:
+                polystr += str(self.coeffpairs[i][1]) + 'x^' + str(self.coeffpairs[i][0]) + '+'
+        return polystr[:-1]
+        
+print("g = SparsePoly([[0,3],[1,5],[6,7]])")
 g = SparsePoly([[0,3],[1,5],[6,7]])
 print(g.degree())
+print(g.printpoly())

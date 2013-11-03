1d8bb344d8191e58f1122d1b075d6fd576efe2e5
David Wilson <mrdavidjwilson@gmail.com>
2013-11-03 14:17:34 +0000

    added print function to dense polynomials

--- polyclasses.py
+++ polyclasses.py
@@ -2,13 +2,26 @@
 
 #dense polynomial
 class DensePoly:
+    'Polynomials stored in a dense manner'
+    coeffs = []
+    
     def __init__(self,coeffs):
         self.coeffs = coeffs
     def degree(self):
         return len(self.coeffs)-1
+    def printpoly(self):
+        polystr = ''
+        n=len(self.coeffs)
+        for i in xrange(1,n):
+            print i
+            polystr += str(self.coeffs[n-i])
+            polystr += 'x^'+str(n-i) + '+'
+        polystr += str(self.coeffs[0])
+        return polystr
 
 f = DensePoly([3,2,5])
 print(f.degree())
+print(f.printpoly())
 
 #Sparse Poly
 class SparsePoly:

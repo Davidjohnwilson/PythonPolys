14e349dc3a23fff51d793dc6523f0558c8d7b90a
David Wilson <mrdavidjwilson@gmail.com>
2016-04-08 11:55:03 +0100

    Another travis fix

--- pypoly/__init__.py
+++ pypoly/__init__.py
@@ -5,5 +5,5 @@ pypoly: Polynomial module for Python
 See https://github.com/Davidjohnwilson/PythonPolys for further documentation.
 """
 
-from .shablona import *  # noqa
+from .pypoly import *  # noqa
 from .version import __version__  # noqa

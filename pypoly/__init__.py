323ade52a91aa34e236e28387cd2428cd035e606
David Wilson <mrdavidjwilson@gmail.com>
2016-04-08 12:00:27 +0100

    Removing testscript from nosetest coverage

--- pypoly/__init__.py
+++ pypoly/__init__.py
@@ -5,5 +5,4 @@ pypoly: Polynomial module for Python
 See https://github.com/Davidjohnwilson/PythonPolys for further documentation.
 """
 
-from .pypoly import *  # noqa
 from .version import __version__  # noqa

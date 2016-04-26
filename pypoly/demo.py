3bd728ae8b4ddd70d75a909b4852b5f140266867
David Wilson <mrdavidjwilson@gmail.com>
2016-04-26 07:57:31 -0700

    SparsePoly in demo

--- pypoly/demo.py
+++ pypoly/demo.py
@@ -9,6 +9,7 @@ from polyclasses import *
 # Demo file for pypoly
 # ==================================
 blank_line = "                                                      "
+full_line = "================================================================"
 
 
 print("# ====================================================== #")
@@ -27,17 +28,38 @@ print("We will demo the DensePoly and SparsePoly classes, including")
 print("Some of their methods.")
 
 print(blank_line)
+print(blank_line)
 
 print("The DensePoly is a dense representation of polynomials. This")
 print("means you simply list the coefficients of ascending powers  ")
 print("of the polynomial.                                          ")
-print("For example, to define 3x^2+2+1 we would use the command:   ")
-print("        f = DensePoly([1, 2, 3])                            ")
+print("For example, to define 3x^2-7x+1 we would use the command:  ")
+print("        f = DensePoly([1, -7, 3])                           ")
 
-f = DensePoly([1, 2, 3])
+f = DensePoly([1, -7, 3])
 
 print("We can check this is the correct polynomial by using the       ")
 print("print_poly command, to give a representation of the polynomial.")
 print(blank_line)
 print("   > f.print_poly()                                            ")
 print("              " + f.print_poly())
+
+print(blank_line)
+print(blank_line)
+
+
+print("The SparsePoly is a sparse representation of polynomials.   ")
+print("This means you list the exponent and coefficient of each    ")
+print("term of the polynomial.                                     ")
+print("For example, to define 3x^2-7x+1 we would use the command:  ")
+print("        g = SparsePoly([[0, 1], [1, -7], [2, 3]])           ")
+
+g = SparsePoly([[0, 1], [1, -7], [2, 3]])
+
+print("We can check this is the correct polynomial by using the       ")
+print("print_poly command, to give a representation of the polynomial.")
+print(blank_line)
+print("   > g.print_poly()                                            ")
+print("              " + g.print_poly())
+
+print(full_line)
